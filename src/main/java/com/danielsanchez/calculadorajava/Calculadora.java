296c9d1eda96a4b3992ff5831a3521fc70c751a2
JD Valeroso <juan.valero.9c@gmail.com>
2024-02-26 10:06:00 -0500

    Se le agrego la raíz a la calculadora en java como a su test

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -34,6 +34,11 @@ public class Calculadora {
                     //operacion modúlo
                     respuesta = numero1 % numero2;
                     break;
+                    case'r':
+                        //operacion raiz
+                      respuesta = Math.pow(numero1,(double)1/numero2);
+                      break;
+                      
             
             
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
@@ -55,7 +60,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /,^,%)");
+        System.out.println("Ingrese la operación (+, -, *, /,^,%,r)");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
